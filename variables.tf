3914bf53b4c078243924f69612c33b645380842e
szpadel <kjunag@wp.pl>
2026-04-07 19:45:07 +0200

    skeleton

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "Region AWS"
+  type        = string
+  default     = "us-east-1"
+}
+variable "project_name" {
+    description = "Common name of freedom project"
+    type = string
+    default = "TODF"
+}
